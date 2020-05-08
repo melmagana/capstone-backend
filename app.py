6f951bcb628410df27b42d7f7de9eb21c69ed381
Melissa Magaña <mmel.mgna@gmail.com>
2020-05-08 15:31:23 -0500

    basic registration works

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
 from flask import Flask
 from resources.accounts import accounts
 import models
+from flask_cors import CORS
+from flask_login import LoginManager
 
 DEBUG=True
 PORT=8000
 
 app = Flask(__name__)
 
+### LoginManager Configuration ###
+## SECRET/KEY FOR SESSIONS ##
+app.secret_key = 'Milo is the best dog in the universe! This is not a secret.'
+## INSTANTIATE LoginManager ##
+login_manager = LoginManager()
+## CONNECT APP WITH THE LOGIN MANAGER ##
+login_manager.init_app(app)
+
+
+
+### CORS -- CROSS ORIGIN RESOURCE SHARING ###
+CORS(accounts, origins=['http://localhost:3000'], supports_credentials=True)
+
+
+
 ### "CONTROLLERS" ###
 app.register_blueprint(accounts, url_prefix='/api/v1/accounts')
+
+
 
 @app.route('/')
 def hello():
--- resources/accounts.py
+++ resources/accounts.py
@@ -1,8 +1,55 @@
 import models
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from flask_bcrypt import generate_password_hash
+from playhouse.shortcuts import model_to_dict
 
 accounts = Blueprint('accounts', 'accounts')
 
 @accounts.route('/')
-def account_index():
+def account_test():
 	return 'accounts resource working'
+
+### REGISTER ROUTE -- POST ###
+@accounts.route('/register', methods=['POST'])
+def register():
+	payload= request.get_json()
+	payload['name'] = payload['name']
+	payload['email'] = payload['email'].lower()
+	print(payload)
+
+	# check if user exists
+	try:
+		models.Account.get(models.Account.email == payload['email'])
+
+		# response
+		return jsonify(
+			data={},
+			message=f"An account with the email {payload['email']} already exists",
+			status=401
+		), 401
+	except models.DoesNotExist:
+
+		# scramble password with bcrypt
+		pw_hash = generate_password_hash(payload['password'])
+
+		# create account
+		create_account = models.Account.create(
+			name=payload['name'],
+			email=payload['email'],
+			password=pw_hash
+		)
+		print('- ' * 30)
+		print('\nthis is create_account:', create_account)
+
+		create_account_dict = model_to_dict(create_account)
+		print('- ' * 30)
+		print('\nthis is create_account_dict:', create_account_dict)
+
+		# remove password
+		create_account_dict.pop('password')
+
+		return jsonify(
+			data=create_account_dict,
+			message=f"Successfully registered account for {create_account_dict['name']}",
+			status=201
+		), 201
